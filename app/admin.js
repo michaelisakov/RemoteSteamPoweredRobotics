9a9352cb6aa4d30d44646532c208b6cf1f81eb14
michaelisakov <michaelsisakov@gmail.com>
2014-04-13 03:31:55 -0400

    Revert "app"
    This reverts commit 5b3d93e26af8786d35ef44d3b80ffb0b8a655ca9.

--- app/admin.js
+++
@@ -1,8 +0,0 @@
-// root => admin.js
-// return an admin panel, render admin.jade, import page model
-
-var Page = require('./models/page.js');
-
-exports.getModel = function() { 
-	
-}
--- app/config/socket.js
+++
@@ -1,8 +0,0 @@
-// config/socket.js
-module.exports = { 
-	connect : { 
-		email : 'HAL > ',
-		command : 'Welcome. You have just connected to the Beagle Bone server',
-		created_at : new Date() 
-	}
-}
